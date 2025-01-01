6a1e26454852371b620ae0e9675326fc00de0172
MrSago <37180898+MrSago@users.noreply.github.com>
2025-01-02 00:10:55 +0300

    chore: update package and guid info

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -6,9 +6,9 @@ internal static class BuildInfo
     public const string DESCRIPTION = "Add native resolution option to the game";
     public const string AUTHOR = "MrS4g0";
     public const string COMPANY = null;
-    public const string VERSION = "1.0.2";
+    public const string VERSION = "1.0.3";
     public const string GAMEVERSION = null;
     public const string DOWNLOADLINK = "https://github.com/MrSago/MiSide-Native-Resolution-Option";
-    public const string PACKAGE = "com.nativeresolutionoption.miside";
-    public const string GUID = "87b48efc-7b1f-4d2d-8105-c6fad0116f84";
+    public const string PACKAGE = "com.miside.nativeresolutionoption";
+    public const string GUID = "3a07a0ca-b691-44e2-ae04-cbb721a7ba09";
 }
